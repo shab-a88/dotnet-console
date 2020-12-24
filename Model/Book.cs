909af9f5fcada757a6441e94e4d6cda462756429
Shaban Ali <shabanali@shabans-mbp.lan>
2020-12-24 12:16:22 +0000

    initial commit

---
+++ Model/Book.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace classes.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello");
+            
+            
+        }
+    }
+}
